daab9053f563e356001ea115a74b5f9b50bbbf50
Marcelo Horikoshi <marcelohorikoshi@gmail.com>
2022-10-04 21:48:47 -0300

    creatio of Login's page and the project has been started

---
+++ src/pages/Login/Login.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import './Login.css'
+
+export default props => {
+  return (
+    <div className="Login">
+      <strong>Página Login teste</strong>
+
+    </div>
+  )
+}
