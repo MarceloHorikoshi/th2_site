ebc5dea0f8c97370d4534fb759dbea84e4906456
Guilherme <guilherme.manzano93@gmail.com>
2022-11-15 01:42:16 -0300

    create forgot password scaffold

---
+++ src/Pages/ForgotPassword/index.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default function ForgotPassword() {
+  return (
+    <div className='ForgotPassword'>
+      <h2>Header</h2>
+
+      <h2>Esqueci a Senha</h2>
+
+      <p>Input Senha</p>
+
+      <p>Recuperar Senha</p>
+
+      <h2>Footer</h2>
+    </div>
+  )
+}
